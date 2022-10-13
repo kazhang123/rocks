8a4a4a8983b6d2fae1133346ac06b5fe31ca3e48
Katharine Zhang <kzed123@students.cs.ubc.ca>
2022-10-13 14:33:44 -0700

    first commit

---
+++ initialize.sh
@@ -0,0 +1,24 @@
+rm -r Documents
+
+mkdir Documents
+mkdir Documents/Archive
+mkdir Documents/Archive/Homework
+mkdir "Documents/Archive/Rock Photos"
+mkdir Documents/Homework
+mkdir Documents/Homework/School2020
+mkdir Documents/Homework/School2020/Math
+touch Documents/Homework/School2020/Math/SFU_math100_Midterm2-2022W1.pdf
+touch Documents/Homework/School2020/Math/SFU_math100_notes.docx
+touch Documents/Homework/School2020/SFU_course-schedule.pdf
+mkdir "Documents/Homework/Special Rocks"
+touch "Documents/Homework/Special Rocks/dwayne-the-rock-johnson.jpg"
+touch "Documents/Homework/Special Rocks/quartz.jpg"
+touch "Documents/Homework/igneous.jpg"
+touch "Documents/Homework/plutonic-rocks.jpg"
+touch "Documents/Homework/sedimentary-rocks.jpg"
+touch "Documents/Homework/SFU_anth103_syllabus.docx"
+touch "Documents/Homework/SFU_biol111_hw0.docx"
+touch "Documents/Homework/UBC_cpsc210_a1.pdf"
+touch "Documents/Homework/UBC_cpsc210_a2.pdf"
+
+
